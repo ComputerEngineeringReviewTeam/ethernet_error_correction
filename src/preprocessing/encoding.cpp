a1b827a2aee662d99948d52c26a96da68e33b3a2
msz <jek_14@wp.pl>
2024-05-11 11:35:34 +0200

    move encodings into separate file

---
+++ src/preprocessing/encoding.cpp
@@ -0,0 +1,146 @@
+#include "encoding.hpp"
+
+namespace encodings {
+    std::uint8_t encodeByte5b6b(std::uint8_t input, int RD) {
+        if (RD == -1)
+            return CODE_5b6b.at(input).first;
+        else if (RD == 1)
+            return CODE_5b6b.at(input).second;
+        else
+            throw std::invalid_argument("RD must be -1 or 1");
+    }
+
+    std::uint8_t encodeByte3b4b(std::uint8_t input, int RD) {
+        if (RD == -1)
+            return CODE_3b4b.at(input).first;
+        else if (RD == 1)
+            return CODE_3b4b.at(input).second;
+        else
+            throw std::invalid_argument("RD must be -1 or 1");
+    }
+
+    int count_ones(symbol10 input) {
+        int count = 0;
+        for (int i = 0; i < 8; i++) {
+            count += (input >> i) & 1;
+        }
+        return count;
+    }
+
+    symbol10 encodeByte8b10b(std::uint8_t input, int RD) {
+        std::uint8_t x = input & EDCBA;
+        std::uint8_t y = input >> 5;
+        std::uint8_t abcdei = encodeByte5b6b(x, RD);
+        if (y == 7) {                                               // check for special case of D.x.P7/D.x.A7
+            if ((RD == -1 && (x == 17 || x == 18 || x == 20)) ||    // see https://en.wikipedia.org/wiki/8b/10b_encoding for explanation
+                (RD ==  1 && (x == 11 || x == 13 || x == 24))) {
+                    y = 8;
+                }
+        }
+        std::uint8_t fghj = encodeByte3b4b(y, RD);
+        symbol10 output = 0;
+        output = (abcdei << 4) | fghj;
+        return output;
+    }
+
+    bytesVec encodeBytesVec8b10b(const bytesVec& data) {
+        if (data.size() % 4 != 0) {
+            throw std::invalid_argument("Data size must be divisible by 4");
+        }
+
+        int RD = -1;
+        bytesVec encoded;               // encoded data, in a vector of bytes (bytesVec)
+        std::uint64_t buffer = 0;       // buffer used to hold 4 10bit symbols before they are saved to encoded bytesVec (as 5 bytes)
+        int bitsInBuffer = 0;           // state of the buffer
+        symbol10 symbol;                // 10bit symbol to encode
+        std::uint8_t invertBuffer[5] ;  // buffer used to invert the order of bytes in the output vector       
+
+        for (int i = 0; i < data.size(); i++) {
+            // encode new symbol and update buffer
+            symbol = encodeByte8b10b(data[i], RD);
+            buffer = (buffer << 10) | symbol;
+            bitsInBuffer += 10;
+
+            // check for running disparity and update RD if needed
+            // '1's in symbol10 (uint16_t really) is counted by count_ones()
+            // '0's in uint16_t would be 16 - count_ones(),
+            // but we are looking for the difference in a 10bit symbol, so 
+            // '0's in symbol10 is 16 - count_ones() - 6 (6 '0's of front-padding)
+            // so the difference is '1's - '0's = count_ones() - (16 - count_ones() - 6) = 2*count_ones() - 10
+            int diff = 2 * count_ones(symbol) - 10;
+            if (diff < 0)
+                RD = -1;
+            else if (diff > 0)
+                RD = 1;
+
+            // if there are 40 bits (4 symbols 10bit each or 5 bytes) in the buffer, save them to encoded bytesVec
+            // the invertBuffer array is used to invert the order of bytes coming into the final buffer
+            if (bitsInBuffer == 40) {
+                for (int j = 0; j < 5; j++) {
+                    invertBuffer[j] = buffer & 0xFF;
+                    buffer = buffer >> 8;
+                    bitsInBuffer -= 8;
+                }
+                for (int j = 4; j >= 0; j--) {
+                    encoded.push_back(invertBuffer[j]);
+                }
+                bitsInBuffer = 0;
+                buffer = 0;
+            }
+        }
+
+        return encoded;
+    }
+
+    std::uint8_t decodeSymbol10b8b(symbol10 symbol) {
+        std::uint8_t abcdei = symbol >> 4;
+        std::uint8_t fghj = symbol & 0b1111;
+        std::uint8_t x=0, y=0;
+        try {
+            x = DECODE_5b6b.at(abcdei);
+        } catch (std::out_of_range& e) {
+        }
+        try {
+            y = DECODE_3b4b.at(fghj);
+        } catch (std::out_of_range& e) {
+        }
+
+        return (y << 5) | x;
+    }
+
+    bytesVec decodeByteVec10b8b(const bytesVec& data) {
+        if (data.size() % 5 != 0) {
+            throw std::invalid_argument("Data size must be divisible by 4");
+        }
+
+        int RD = -1;
+        bytesVec decoded;               // decoded data, in a vector of bytes (bytesVec)
+        std::uint64_t buffer = 0;       // buffer used to hold 5 bytes (4 10 bit symbols) before they are decoded and saved to decoded bytesVec
+        int bitsInBuffer = 0;           // state of the buffer
+        symbol10 symbol;                // 10bit symbol to decode
+        std::uint8_t invertBuffer[4];   // buffer used to invert the order of bytes in the output vector
+
+        for (int i = 0; i < data.size(); i++) {
+            // push bytes into buffer
+            buffer = (buffer << 8) | data[i];
+            bitsInBuffer += 8;
+            
+            // after there are 40 bits (5 bytes, 4 symbols 10bit each) in the buffer
+            // decode each symbol and save decoded bytes to decoded bytesVec
+            if (bitsInBuffer == 40) {
+                for (int j = 0; j < 4; j++) {
+                    symbol = 0;
+                    symbol = buffer & 0b0000001111111111;
+                    buffer = buffer >> 10;
+                    bitsInBuffer -= 10;
+                    invertBuffer[j] = decodeSymbol10b8b(symbol);
+                }
+                for (int j = 3; j >= 0; j--) {
+                    decoded.push_back(invertBuffer[j]);
+                }
+            }
+        }
+
+        return decoded;
+    }
+};
---
+++ src/preprocessing/encoding.hpp
@@ -0,0 +1,128 @@
+// Author: Marek Szymański
+// Description: 
+
+#pragma once
+
+#include <vector>
+#include <cstdint>
+#include <stdexcept>
+
+#include "encodingtables.h"
+
+using byte = std::uint8_t;
+using bytesVec = std::vector<byte>;
+using symbol10 = std::uint16_t;
+
+namespace encodings {
+    /**
+     * @brief Bit mask to extract the younger 5 bits from byte
+     * 
+     */
+    const std::uint8_t EDCBA = 0b00011111;
+    /**
+     * @brief Bit mask to extract the older 3 bits from byte
+     * 
+     */
+    const std::uint8_t HGF   = 0b11100000;
+
+    /**
+     * @brief 5b/6b encoding of a single symbol
+     * @details Encodes 5 bits to 6 bits, using the 5b/6b encoding table, with th respect of running disparity (used in 8b/10b encoding)
+     *          The table, defined in encodingtables.h, is a map of pairs of 6bit symbols, former for RD=-1 and latter for RD=1
+     *          As it's not really feasible to pass around packets of 5 or 6 bits, the function takes 8bit input and returns 8bit output
+     *          the "additional" bits are filled with '0's 
+     *          Throws std::invalid_argument if RD is neither -1 nor 1
+     * 
+     * @param input         - symbol to encode - uint8_t used to transport 5 bit symbol of 5b/6b encoding, with the "additional" bits are set to 0
+     * @param RD            - running disparity, -1 or 1
+     * @return std::uint8_t - encoded symbol - uint8_t used to transport 6 bit symbol of 5b/6b encoding, with the "additional" bits are set to 0
+     */
+    std::uint8_t encodeByte5b6b(std::uint8_t input, int RD);
+
+    /**
+     * @brief 3b/4b encoding of a single symbol
+     * @details Encodes 3 bits to 4 bits, using the 3b/4b encoding table, with th respect of running disparity (used in 8b/10b encoding)
+     *          The table, defined in encodingtables.h, is a map of pairs of 4bit symbols, former for RD=-1 and latter for RD=1
+     *          As it's not really feasible to pass around packets of 3 or 4 bits, the function takes 8bit input and returns 8bit output
+     *          the "additional" bits are filled with '0's 
+     *          Throws std::invalid_argument if RD is neither -1 nor 1
+     * 
+     * @param input         - symbol to encode - uint8_t used to transport 3 bit symbol of 3b/4b encoding, with the "additional" bits are set to 0
+     * @param RD            - running disparity, -1 or 1
+     * @return std::uint8_t - encoded symbol - uint8_t used to transport 4 bit symbol of 3b/4b encoding, with the "additional" bits are set to 0
+     */
+    std::uint8_t encodeByte3b4b(std::uint8_t input, int RD);
+
+    /**
+     * @brief Counts high bits ('1') in the given 10bit symbol
+     * @details While such implementation is obviously inefficient, it is used to avoid relying on compiler's built-in functions
+     * 
+     * @param input - 10bit symbol to count high bits in
+     * @return int  - number of high bits in the symbol
+     */
+    int count_ones(symbol10 input);
+
+    /**
+     * @brief 8b/10b encoding of a single symbol
+     * @details Encodes 8 bits to 10 bits, using 5b/6b and 3b/4b encodings
+     *          See https://en.wikipedia.org/wiki/8b/10b_encoding for more detailed explanation of the algorithm
+     *          Uses encodeByte5b6b() to encode the younger 5 bits of the input symbol into the older 6 bits of the output symbol
+     *          and encodeByte3b4b() to encode the older 3 bits of the input symbol into the younger 4 bits of the output symbol
+     *          In order to avoid long series of 0 or 1 the 8b/10b encoding tracks the "running disparity" (RD) 
+     *          RD is the difference between the number of '1' and '0' bits in the previous encoded symbol
+     *          and is used to choose the correct 5b/6b and 3b/4b encoding of the next symbol
+     * 
+     * @param input     - symbol to encode - uint8_t used to transport 8 bit symbol of 8b/10b encoding, with the "additional" bits are set to 0
+     * @param RD        - running disparity, -1 or 1
+     * @return symbol10 - uint16_t used to transport 10 bit symbol of 8b/10b encoding, with the "additional" bits are set to 0
+     */
+    symbol10 encodeByte8b10b(std::uint8_t input, int RD);
+
+    /**
+     * @brief Encodes vector of bytes to 8b/10b encoded vector of bytes, the given vector MUST have length divisible by 4
+     * @details Encodes gicen vector of bytes (bytesVec) with the 8b/10b encoding
+     *          see https://en.wikipedia.org/wiki/8b/10b_encoding for more detailed explanation
+     *          see encodeByte8b10b() for how single symbol is encoded
+     *          The 10 bit encoded symbols are buffered until 40 bits (4 symbols) are stored
+     *          so that 5 bytes can be written to output vector at once
+     *          (the bytes have to be written in reverse order, as the 10bit symbols are stored in reverse order in the buffer)
+     *          due to this the vector being encoded must have length divisible by 4 
+     *          throws std::invalid_argument otherwise.
+     *          The function also tracks the "running disparity" (RD) of the encoded symbols
+     *          and updates it accordingly
+     * 
+     * @param data      - vector of bytes to encode, must have length divisible by 4, not modified
+     * @return bytesVec - 8b/10b encoded vector of bytes
+     */
+    bytesVec encodeBytesVec8b10b(const bytesVec& data);
+
+    /**
+     * @brief Decodes 10bit symbol to 8bit symbol
+     * @details Decodes 10bit symbol to 8bit symbol, using 5b/6b and 3b/4b decodings
+     *          See https://en.wikipedia.org/wiki/8b/10b_encoding for more detailed explanation of the algorithm
+     *          Uses DECODE_5b6b and DECODE_3b4b tables to decode the 10bit symbol
+     *          The function first extracts the younger 6 bits of the 10bit symbol and decodes them using DECODE_5b6b table
+     *          Then it extracts the older 4 bits of the 10bit symbol and decodes them using DECODE_3b4b table
+     *          The decoded 6 and 4 bits are then combined into 8bit symbol
+     * 
+     * @param symbol        - 10bit symbol to decode
+     * @return std::uint8_t - 8bit decoded symbol
+     */
+    std::uint8_t decodeSymbol10b8b(symbol10 symbol);
+
+    /**
+     * @brief Decode vector of bytes from 8b/10b encoded vector of bytes, the given vector MUST have length divisible by 5
+     * @details Decodes given vector of bytes (bytesVec) with the 8b/10b encoding
+     *          see decodeSymbol10b8b() for how single symbol is decoded
+     *          The 10 bit encoded symbols are buffered until 40 bits (4 symbols) are stored
+     *          so that 5 bytes can be written to output vector at once
+     *          (the bytes have to be written in reverse order, as the 10bit symbols are stored in reverse order in the buffer)
+     *          due to this the vector being decoded must have length divisible by 5
+     *          throws std::invalid_argument otherwise.
+     *          During decoding 8b/10b encoding there is no need to track the "running disparity" (RD) of the encoded symbols
+     * 
+     * @param data      - vector of bytes encided in 8b/10b to decode, must have length divisible by 4, not modified
+     * @return bytesVec - decoded vector of bytes
+     */
+    bytesVec decodeByteVec10b8b(const bytesVec& data);
+};
